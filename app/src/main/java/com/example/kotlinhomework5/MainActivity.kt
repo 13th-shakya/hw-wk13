d8134e29f5da836a312e141de9f5a508c886859f
Riley Ho <riley@rileychh.dev>
2024-12-07 16:03:31 +0800

    feat: scaffold MainActivity

--- app/src/main/java/com/example/kotlinhomework5/MainActivity.kt
+++ app/src/main/java/com/example/kotlinhomework5/MainActivity.kt
@@ -6,8 +6,10 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.kotlinhomework5.databinding.ActivityMainBinding
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,5 +22,17 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
+    override fun onMapReady(map: GoogleMap) {
+        TODO("Not yet implemented")
+    }
+
+    private fun loadMap() {
+        TODO("Not yet implemented")
     }
 }

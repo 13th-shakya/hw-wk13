0e6a7584205675c6697b9c4f992bfac8d242806f
Riley Ho <riley@rileychh.dev>
2024-12-07 16:47:22 +0800

    fix: bump target SDK to 35

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.kotlinhomework5"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.kotlinhomework5"
         minSdk = 33
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 

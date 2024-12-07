3edd63ade62cf9b6b421a9af85113f6b8ed8bcd1
Riley Ho <riley@rileychh.dev>
2024-12-07 16:16:41 +0800

    feat: implement permission requesting with early return pattern

--- app/src/main/java/com/example/kotlinhomework5/MainActivity.kt
+++ app/src/main/java/com/example/kotlinhomework5/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.example.kotlinhomework5
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.kotlinhomework5.databinding.ActivityMainBinding
@@ -22,13 +25,40 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        loadMap()
     }
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        val allGranted = grantResults.isNotEmpty() && grantResults.all {
+            it == PackageManager.PERMISSION_GRANTED
+        }
+
+        if (allGranted) {
+            loadMap()
+        } else {
+            finish()
+        }
     }
 
     override fun onMapReady(map: GoogleMap) {
+        val isAccessFineLocationGranted = ActivityCompat.checkSelfPermission(
+            this, Manifest.permission.ACCESS_FINE_LOCATION
+        ) == PackageManager.PERMISSION_GRANTED
+
+        val isAccessCoarseLocationGranted = ActivityCompat.checkSelfPermission(
+            this, Manifest.permission.ACCESS_COARSE_LOCATION
+        ) == PackageManager.PERMISSION_GRANTED
+
+        if (!isAccessFineLocationGranted || !isAccessCoarseLocationGranted) {
+            ActivityCompat.requestPermissions(
+                this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 0
+            )
+            return
+        }
+
         TODO("Not yet implemented")
     }
 
